369a91a9048cd0de9cce769f53e213ca885a8312
Gurjot Kooner <226573228+koonergurjot@users.noreply.github.com>
2025-09-29 09:36:15 -0700

    Use relative asset URLs for PWA resources

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE='cmatrix-v4_5';
-const ASSETS=['/','/index.html','/manifest.webmanifest','/styles.css','/icon-192.svg','/icon-512.svg'];
+const ASSETS=['./','index.html','manifest.webmanifest?v=1','styles.css?v=1','icon-192.svg?v=1','icon-512.svg?v=1'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))) });
 self.addEventListener('fetch',e=>{ 
@@ -16,7 +16,7 @@ self.addEventListener('fetch',e=>{
         return r; 
       }).catch(()=>{
         if(url.pathname.startsWith('/')) {
-          return caches.match('/index.html');
+          return caches.match('index.html').then(res=>res||caches.match('./'));
         }
         return Promise.reject('offline');
       });
