1fe82ba631dd9898a2417e455cad5642e0f71d1c
Gurjot Kooner <226573228+koonergurjot@users.noreply.github.com>
2025-10-17 08:48:10 -0700

    Add fullscreen toggle to import drawer

--- src/main.js
+++ src/main.js
@@ -963,6 +963,7 @@ const v2DashboardAppDefinition = () => ({
   lastCloudSync: '',
   importDrawer: {
     open: false,
+    pcFullscreen: false,
     mode: 'employees',
     file: null,
     fileName: '',
@@ -1347,6 +1348,7 @@ const v2DashboardAppDefinition = () => ({
     this.importDrawer.commitDisabled = true;
     this.importDrawer.commitLocalDisabled = true;
     this.importDrawer.submitDisabled = true;
+    this.importDrawer.pcFullscreen = false;
     this.updateImportDrawerCommitState();
     if (this.$refs.importFileInput) {
       this.$refs.importFileInput.value = '';
